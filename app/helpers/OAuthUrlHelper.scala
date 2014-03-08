314a8e892a7ecb2b73f6d4cec283b6385d3ab2c3
Kevin Hyland <khy@me.com>
2014-03-08 12:44:40 -0500

    Add OAuthUrlHelper

---
+++ app/helpers/OAuthUrlHelper.scala
@@ -0,0 +1,15 @@
+package helpers
+
+import play.api.Play
+
+object OAuthUrlHelper {
+
+  lazy val config = Play.current.configuration
+
+  def uselessUrl = {
+    val baseUrl = config.getString("useless.auth.baseUrl").get
+    val appGuid = config.getString("useless.account.guid").get
+    baseUrl + s"/auth?app_guid=$appGuid"
+  }
+
+}
