b26f037dbaaec6954de8c7a43c41abcde1197262
Kevin Hyland <khy@me.com>
2014-03-07 21:13:24 -0500

    Add an explanation of AccessTokenService.ensureAccessToken

--- app/services/AccessTokenService.scala
+++ app/services/AccessTokenService.scala
@@ -8,6 +8,26 @@ import clients.AuthClient
 
 class AccessTokenService(authClient: AuthClient) {
 
+  /**
+   * If an access token corresponding to the specified code exists, this method
+   * ensures that it is added to some account document, unless it has been
+   * already. If the code does not belong to an access token, a Left is returned.
+   *
+   * If an Account instance is specified, the returned access token is
+   * guaranteed to belong to that account.
+   *
+   * The possible outcomes of this method are:
+   *   * an existing access token is returned.
+   *   * an access token is added to an existing account, and is returned.
+   *   * a new account is created, an access token is added to it, and is
+   *     returned.
+   *
+   * It also takes care not to use the same code to retrieve an access token
+   * more than once, since OAuth suggests that the server delete access tokens
+   * for which this occurs. Thread safety in this respect is left up to the
+   * AuthClient. So, concurrent requests for a single code MAY result in
+   * multiple requests, depending upon the implementation of the AuthClient.
+   */
   def ensureAccessToken(
     code: String,
     optAccount: Option[Account] = None
