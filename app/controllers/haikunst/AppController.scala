3a5e869f3a332f9119b0e55742cea8d4b1f84d70
Kevin Hyland <khy@me.com>
2014-04-04 22:56:21 -0400

    Basic Haikunst! layout

---
+++ app/controllers/haikunst/AppController.scala
@@ -0,0 +1,14 @@
+package controllers.haikunst
+
+import play.api._
+import play.api.mvc._
+
+import controllers.auth.AuthAction._
+
+object AppController extends Controller {
+
+  def index = Action {
+    Ok(views.html.haikunst.index())
+  }
+
+}
