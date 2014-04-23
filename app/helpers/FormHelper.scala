604b7a3649cffaa13b3dee559935f0af80aab91f
Kevin Hyland <khy@me.com>
2014-04-22 22:51:18 -0400

    Improve FormHelper.displayFirstError

--- app/helpers/FormHelper.scala
+++ app/helpers/FormHelper.scala
@@ -1,9 +1,17 @@
 package helpers
 
 import play.api.data.Form
+import play.api.i18n.Messages
 
 object FormHelper {
 
-  def displayError[T](form: Form[T]) = form.errors.head.message
+  def displayFirstError[T](form: Form[T]) = {
+    val formError = form.errors.head
+
+    formError.message match {
+      case "error.required" => Messages("error.key-required", formError.key.capitalize)
+      case _ => Messages(formError.message)
+    }
+  }
 
 }
