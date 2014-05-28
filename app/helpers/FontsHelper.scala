4cc2afa5cc495ffdc7114a5b9071a77fae0a0561
Kevin Hyland <khy@me.com>
2014-05-27 23:17:05 -0400

    Avoid mixed content warnings for fonts in prod

---
+++ app/helpers/FontsHelper.scala
@@ -0,0 +1,13 @@
+package helpers
+
+import play.api.Play
+
+object FontsHelper {
+
+  lazy val config = Play.current.configuration
+
+  def stylesheetUrl(family: String) = {
+    config.getString("google.fonts.baseUrl").get + "/css?family=" + family
+  }
+
+}
