88bc5db1cb1bfae5144edb21b4773c94ce2c5ea8
Kevin Hyland <khyland@gilt.com>
2015-02-07 12:02:27 -0500

    Fix whitespace in Defaults.scala

--- project/Defaults.scala
+++ project/Defaults.scala
@@ -12,13 +12,13 @@ object Defaults {
     val useless = "io.useless"  %% "useless"  % "0.16.0"
 
     val webJars = Seq(
-      "org.webjars"       % "requirejs"      % "2.1.15",
-      "org.webjars"       % "jquery"         % "2.1.1",
-      "org.webjars"       % "normalize.css"  % "3.0.1",
-      "org.webjars"       % "underscorejs"   % "1.7.0",
-      "org.webjars"       % "backbonejs"     % "1.1.2-2",
-      "org.webjars"       % "handlebars"     % "2.0.0-alpha.2",
-      "org.webjars"       % "momentjs"       % "2.8.3"
+      "org.webjars" % "requirejs"      % "2.1.15",
+      "org.webjars" % "jquery"         % "2.1.1",
+      "org.webjars" % "normalize.css"  % "3.0.1",
+      "org.webjars" % "underscorejs"   % "1.7.0",
+      "org.webjars" % "backbonejs"     % "1.1.2-2",
+      "org.webjars" % "handlebars"     % "2.0.0-alpha.2",
+      "org.webjars" % "momentjs"       % "2.8.3"
     )
 
     val resolvers = Seq(
