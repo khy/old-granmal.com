2cb2861270bee1318523492f3e1027ce9f05c8ab
Kevin Hyland <khy@me.com>
2015-05-12 22:44:34 -0400

    Incude Typesafe repository by default

--- project/Defaults.scala
+++ project/Defaults.scala
@@ -24,6 +24,7 @@ object Defaults {
 
     val resolvers = Seq(
       "Local Ivy"               at "file://" + Path.userHome.absolutePath + "/.ivy2/local",
+      "Typesafe Releases"       at "http://repo.typesafe.com/typesafe/releases/",
       "Sonatype OSS Snapshots"  at "https://oss.sonatype.org/content/repositories/snapshots",
       "Sonatype OSS Releases"   at "https://oss.sonatype.org/content/groups/public"
     )
